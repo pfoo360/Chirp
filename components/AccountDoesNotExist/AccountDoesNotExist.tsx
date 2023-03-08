5c905ba442af5730040ae8ed34cb1a413006ea54
Phillip Foo <pfoo360@gmail.com>
2023-03-08 00:44:00 -0500

    added Head components, fixed apostrophe escape character errors

--- components/AccountDoesNotExist/AccountDoesNotExist.tsx
+++ components/AccountDoesNotExist/AccountDoesNotExist.tsx
@@ -8,7 +8,7 @@ const AccountDoesNotExist: FC = () => {
       <div className="w-full flex flex-col items-center mt-14">
         <div className="w-3/5">
           <p className="font-extrabold text-gray-900 px-5 text-3xl">
-            This account doesn't exist
+            {`This account doesn't exist`}
           </p>
           <p className="font-normal text-gray-500 px-5 text-base mt-1">
             Try searching for another.
